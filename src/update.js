a60d4feed5fecad05edd2f082a3926b511e7eb48
Landan Ginther <ginther4280@live.missouristate.edu>
2020-01-31 17:59:49 -0600

    Delete update.js

--- src/update.js
+++
@@ -1,5 +0,0 @@
-export default function update() {
-    var hello_world = document.createElement("P"); 
-    hello_world.innerText = "Hello World!"; 
-    document.body.appendChild(hello_world); 
-}
