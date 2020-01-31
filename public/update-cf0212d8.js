2bd77534649dd9fd14dcd83251f7e83d5795bc03
Landan Ginther <ginther4280@live.missouristate.edu>
2020-01-31 17:37:56 -0600

    Adding needed files and creating "Hello World!" message

---
+++ public/update-cf0212d8.js
@@ -0,0 +1,2 @@
+export default function(){var e=document.createElement("P");e.innerText="Hello World!",document.body.appendChild(e)}
+//# sourceMappingURL=update-cf0212d8.js.map
---
+++ src/update.js
@@ -0,0 +1,5 @@
+export default function update() {
+    var hello_world = document.createElement("P"); 
+    hello_world.innerText = "Hello World!"; 
+    document.body.appendChild(hello_world); 
+}
