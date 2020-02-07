70919cc6cc1bd065220cc31951b3e4164510ba9c
Landan Ginther <landanginther25@gmail.com>
2020-02-07 12:51:54 -0600

    Adding JS for links between pages in index.html.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
 
-var categories = document.getElementById("categories"); 
-categories.addEventListener('click', function(event) {
+var categories = document.getElementById('categories');
+categories.onclick = function(event) {
     window.location.href = "categories.html";
-}, false); 
+};
+
+let albums = ['all-photos', 'recent-photos', 'favorites', 'trip-to-austria', 'selfies'];
+
+albums.forEach(element => {
+    var album = document.getElementById(element); 
+    album.onclick = function(event) {
+        window.location.href = "album.html"; 
+    };
+});
+
+let photos = ['photo-1', 'photo-2', 'photo-3', 'photo-4', 'photo-5'];
+
+photos.forEach(element => {
+    var img = document.getElementById(element);
+    img.onclick = function(event) {
+        window.location.href = "image.html";
+    }
+});
