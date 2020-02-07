17f9480321314fad3f6f7c453744506c510bff9f
Landan Ginther <landanginther25@gmail.com>
2020-02-07 15:42:48 -0600

    Adding JS frame for more implementation.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,39 @@
 console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
 
-var categories = document.getElementById('categories');
-categories.onclick = function(event) {
-    window.location.href = "categories.html";
-};
+var main_page = document.getElementById('main');
+var categories_page = document.getElementById('categories'); 
+var album_page = document.getElementById('album')
 
-let albums = ['all-photos', 'recent-photos', 'favorites', 'trip-to-austria', 'selfies'];
-
-albums.forEach(element => {
-    var album = document.getElementById(element); 
-    album.onclick = function(event) {
-        window.location.href = "album.html"; 
+if (main_page)
+{
+    var categories = document.getElementById('categories');
+    categories.onclick = function(event) {
+        window.location.href = "categories.html";
     };
-});
 
-let photos = ['photo-1', 'photo-2', 'photo-3', 'photo-4', 'photo-5'];
+    let albums = ['all-photos', 'recent-photos', 'favorites', 'trip-to-austria', 'selfies'];
 
-photos.forEach(element => {
-    var img = document.getElementById(element);
-    img.onclick = function(event) {
-        window.location.href = "image.html";
-    }
-});
+    albums.forEach(element => {
+        var album = document.getElementById(element); 
+        album.onclick = function(event) {
+            window.location.href = "album.html"; 
+        };
+    });
+
+    let photos = ['photo-1', 'photo-2', 'photo-3', 'photo-4', 'photo-5'];
+
+    photos.forEach(element => {
+        var img = document.getElementById(element);
+        img.onclick = function(event) {
+            window.location.href = "image.html";
+        }
+    });
+}
+else if (categories_page)
+{
+
+}
+else if (album_page)
+{
+    
+}
