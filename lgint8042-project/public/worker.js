51bfb232cf83cfc1f2e3dcfe4c77780836ca8e17
Landan Ginther <landanginther25@gmail.com>
2020-03-06 12:15:18 -0600

    Add service worker.

---
+++ lgint8042-project/public/worker.js
@@ -0,0 +1,36 @@
+var CACHE_NAME = 'lgint8042-project';
+var urlsToCache = [
+    '/',
+    '/album',
+    '/categories',
+    'image'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(function(response) {
+        if (response) {
+            return response
+        }
+        return fetch(event.request);
+    }))
+});
+
+self.addEventListener('activate', event => {
+    var cacheWhiteList = ['lgint8042-project'];
+    event.waitUntil(caches.keys().then(cacheNames => {
+        return Promise.all(cacheNames.map(cacheName => {
+            if (cacheWhiteList.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName);
+            }
+        }))
+    }));
+});
